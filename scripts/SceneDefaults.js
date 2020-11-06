632bd7c96867bbc4e8cff374a032f199c784b400
Ionut Hada <hadaionut@gmail.com>
2020-11-06 12:52:54 +0200

    Fixed saving data only on second opening of the sceneConfig app

--- scripts/SceneDefaults.js
+++ scripts/SceneDefaults.js
@@ -1,35 +1,49 @@
-import {registerSettings, setSetting} from "./Setting.js";
+import {registerSettings, setSetting, getSetting} from "./Setting.js";
 
-Hooks.on('init', () => {
-    registerSettings();
+Hooks.on('init', () => registerSettings());
 
-})
-
-const saveSceneData = (entityData) => async () => {
-    delete entityData._id;
-    delete entityData.name;
-    await setSetting(entityData, 'defaultSettings');
+const getSceneData = (entityData) => {
+    const newEntityData = JSON.parse(JSON.stringify(entityData._getSubmitData()));
+    delete newEntityData._id;
+    delete newEntityData.name;
+    delete newEntityData['initial.x'];
+    delete newEntityData['initial.y'];
+    delete newEntityData['initial.scale'];
+    return newEntityData;
 }
 
+const saveSceneData = (entityData) => async () => await setSetting(getSceneData (entityData), 'defaultSettings');
+
 const createNewButton = (sceneSave) => {
-    let button = $('<button>Save as default</button>');
+    let button = $('<button  type="button" name="submit2">Save as default</button>');
     button.on('click', sceneSave)
     return button;
 }
 
 const remakeButtonsStructure = (sceneSave) => {
     let newButtonStructure = $('<div class="form-group"></div>');
-    newButtonStructure.append($('button[name ="submit"]'), createNewButton(sceneSave));
+    newButtonStructure.append($('button[name="submit"]'), createNewButton(sceneSave));
 
     return newButtonStructure;
 }
 
-const getButtonLocation = ($form) => $form.children('section').children()
+const getButtonLocation = ($form) => $form.children('section').children();
+
+const isADefaultSaved = () => !!getSetting('defaultSettings');
+
+const updateSceneData = (sceneData) => {
+    const savedSettings = getSetting('defaultSettings');
+    Object.keys(savedSettings).forEach((savedDataKey)=> sceneData[savedDataKey] = savedSettings[savedDataKey])
+}
 
 Hooks.on('renderSceneConfig', async (html, $form, formData) => {
-    const sceneSave = saveSceneData(formData.entity);
+    console.log()
+    const sceneSave = saveSceneData(html);
     const buttonStructure = remakeButtonsStructure(sceneSave);
 
     getButtonLocation($form).append(buttonStructure);
-    console.log(html, $form);
+})
+
+Hooks.on('preCreateScene', (scenedata) => {
+    if (isADefaultSaved()) updateSceneData(scenedata);
 })
